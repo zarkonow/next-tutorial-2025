9386457d5a6653a09189c5c9cc698f75b847147b
zarkonow <stojanov1987@gmail.com>
2025-02-26 18:22:07 +0100

    f\\First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Hello World.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
