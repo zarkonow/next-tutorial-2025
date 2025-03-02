7050d64038cd903a4ee0f07236472ffb9b19e8d8
zarkonow <stojanov1987@gmail.com>
2025-03-02 21:17:31 +0100

    feat: add blog components and improve formatting in existing pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-  return (
-    <div>About</div>
-  )
+
+
+
+  
+  return <div>About</div>;
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function MyProfile() {
-  return (
-    <div>My Profile</div>
-  )
+  return <div>My Profile</div>;
 }
