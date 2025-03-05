e83cdcde5e1ece79144c52bea8cbd9e50a3ec247
zarkonow <stojanov1987@gmail.com>
2025-03-05 18:07:53 +0100

    feat: add Docs component to display documentation based on feature and version

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default async function Docs({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
+}) {
+  const {slug} = await params;
+
+  if(slug.length == 2) {
+    return(
+      <h1>
+        Viewing docs for feature {slug[0]} and version {slug[1]}
+      </h1>
+    )
+  }else if (slug?.length == 1) {
+    return(
+      <h1>
+        Viewing docs for feature {slug[0]}
+      </h1>
+    )
+
+  }
+
+  return <div>Docs page</div>;
+}
